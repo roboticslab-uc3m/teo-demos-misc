df2b22651a4b94c30d8a1e71c7c7b723dfbc7eaa
jgvictores <jcgvicto@gmail.com>
2019-03-17 16:08:45 +0100

    more arms

--- teo-demo-2019-master.py
+++ teo-demo-2019-master.py
@@ -8,12 +8,14 @@ robot = '/teoSim'
 
 from time import sleep
 
+# YARP
 import yarp  # imports YARP
 yarp.Network.init()  # connect to YARP network
 if yarp.Network.checkNetwork() != True:  # let's see if there was actually a reachable YARP network
     print '[error] Please try running yarp server'  # tell the user to start one with 'yarp server' if there isn't any
     quit()
 
+#-- Left Arm (LA)
 optionsLA = yarp.Property()  # create an instance of Property, a nice YARP class for storing name-value (key-value) pairs
 optionsLA.put('device','remote_controlboard')  # we add a name-value pair that indicates the YARP device
 optionsLA.put('remote',robot+'/leftArm')  # we add info on to whom we will connect
@@ -24,10 +26,22 @@ posLA = ddLA.viewIPositionControl()  # make a position controller object we call
 modeLA = ddLA.viewIControlMode2()  # make a operation mode controller object we call 'mode'
 
 axesLA = posLA.getAxes()  # retrieve number of joints
+modeLA.setControlModes(yarp.IVector(axesLA, yarp.encode('pos'))) # note: stops the robot
 
-# use the object to set the device to position mode (as opposed to velocity mode)(note: stops the robot)
-modeLA.setControlModes(yarp.IVector(axesLA, yarp.encode('pos')))
+#-- Right Arm (RA)
+optionsRA = yarp.Property()  # create an instance of Property, a nice YARP class for storing name-value (key-value) pairs
+optionsRA.put('device','remote_controlboard')  # we add a name-value pair that indicates the YARP device
+optionsRA.put('remote',robot+'/rightArm')  # we add info on to whom we will connect
+optionsRA.put('local','/demo'+robot+'/rightArm')  # we add info on how we will call ourselves on the YARP network
+ddRA = yarp.PolyDriver(optionsRA)  # create a YARP multi-use driver with the given options
 
+posRA = ddRA.viewIPositionControl()  # make a position controller object we call 'pos'
+modeRA = ddRA.viewIControlMode2()  # make a operation mode controller object we call 'mode'
+
+axesRA = posRA.getAxes()  # retrieve number of joints
+modeRA.setControlModes(yarp.IVector(axesRA, yarp.encode('pos'))) # note: stops the robot
+
+#-- Program
 posLA.positionMove(0,-35)
 while not posLA.checkMotionDone():
     sleep(0.1)

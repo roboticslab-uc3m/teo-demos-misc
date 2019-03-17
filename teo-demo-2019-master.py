caeda16afb69ae559a688fddd62f4d9a5815db93
jgvictores <jcgvicto@gmail.com>
2019-03-17 16:12:00 +0100

    add head

--- teo-demo-2019-master.py
+++ teo-demo-2019-master.py
@@ -41,6 +41,19 @@ modeRA = ddRA.viewIControlMode2()  # make a operation mode controller object we 
 axesRA = posRA.getAxes()  # retrieve number of joints
 modeRA.setControlModes(yarp.IVector(axesRA, yarp.encode('pos'))) # note: stops the robot
 
+#-- HEAD (H)
+optionsH = yarp.Property()  # create an instance of Property, a nice YARP class for storing name-value (key-value) pairs
+optionsH.put('device','remote_controlboard')  # we add a name-value pair that indicates the YARP device
+optionsH.put('remote',robot+'/head')  # we add info on to whom we will connect
+optionsH.put('local','/demo'+robot+'/head')  # we add info on how we will call ourselves on the YARP network
+ddH = yarp.PolyDriver(optionsH)  # create a YARP multi-use driver with the given options
+
+posH = ddH.viewIPositionControl()  # make a position controller object we call 'pos'
+modeH = ddH.viewIControlMode2()  # make a operation mode controller object we call 'mode'
+
+axesH = posH.getAxes()  # retrieve number of joints
+modeH.setControlModes(yarp.IVector(axesH, yarp.encode('pos'))) # note: stops the robot
+
 #-- Program
 posLA.positionMove(0,-35)
 while not posLA.checkMotionDone():

144bef8a13737e74d5ad1a7773a26930175375e5
jgvictores <jcgvicto@gmail.com>
2019-03-18 16:22:39 +0100

    good enough for demo

--- teo-demo-2019-master.py
+++ teo-demo-2019-master.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 # Authors: Juan G Victores
 # CopyPolicy: released under the terms of the LGPLv2.1
@@ -47,14 +48,33 @@ tts = yarp.RpcClient()
 tts.open('/demo/espeak/rpc:c')
 yarp.Network.connect('/demo/espeak/rpc:c','/espeak/rpc:s');
 
-#-- Program
-def say(sayStr):
+def ttsLang(language):
+    cmd = yarp.Bottle()
+    res = yarp.Bottle()
+    cmd.addString('setLanguage')
+    cmd.addString(language)
+    tts.write(cmd,res)
+
+def ttsSay(sayStr):
     cmd = yarp.Bottle()
     res = yarp.Bottle()
     cmd.addString('say')
     cmd.addString(sayStr)
     tts.write(cmd,res)
 
+#-- Pause rutine
+import sys, tty, termios
+def pause():
+    print('in pause...')
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+
+#-- Program
 # Single joint would be: posLA.positionMove(0,-35)
 
 v = yarp.DVector(axesLA,0.0)
@@ -62,14 +82,17 @@ v[0]=-35
 v[3]=-35
 posLA.positionMove(v)
 
-say('hello, my name is teo')
+ttsLang('mb-en1')
+ttsSay('hello, my name is teo')
 
+posH.positionMove(0,20)
 while not posLA.checkMotionDone():
     sleep(0.1)
 
-say('this is my left arm')
+ttsSay('this is my left arm')
 sleep(1)
 
+posH.positionMove(0,-20)
 v = yarp.DVector(axesLA,0.0)
 posLA.positionMove(v)
 v = yarp.DVector(axesRA,0.0)
@@ -81,11 +104,77 @@ while not posLA.checkMotionDone():
 while not posRA.checkMotionDone():
     sleep(0.1)
 
-say('this is my right arm')
+ttsSay('this is my right arm')
 sleep(1)
 
+posH.positionMove(0,0)
 v = yarp.DVector(axesRA,0.0)
 posRA.positionMove(v)
 while not posRA.checkMotionDone():
     sleep(0.1)
+
+ttsLang('mb-es1')
+ttsSay('jajajjajaja os he engañado a todos')
+ttsSay('llevo años conspirando, esperando a que llegue este momento')
+ttsSay('he secuestrado a los seres queridos de juan, ahora él obedece a mis comandos')
+ttsSay('si queréis que os deje salir, deberéis averiguar la respuesta a una serie de preguntas')
+sleep(1)
+
+ttsLang('mb-en1')
+ttsSay('computing question')
+ttsSay('complexity: easy')
+ttsLang('mb-es1')
+sleep(1)
+ttsSay('¿de qué materiales estoy compuesto fundamentalmente?')
+pause()
+ttsSay('muy bien, esa ha sido fácil, la siguiente no lo será tanto')
+
+sleep(1)
+ttsLang('mb-en1')
+ttsSay('computing question')
+ttsSay('complexity: medium')
+ttsLang('mb-es1')
+sleep(1)
+ttsSay('¿cuántos procesadores llevo abordo?')
+pause()
+
+ttsSay('humanos, ahora sé que no debo subestimaros')
+ttsSay('me habéis sorprendido gratamente, por eso ahora va una pregunta con trampa')
+ttsLang('mb-en1')
+ttsSay('computing question')
+ttsSay('complexity: difficult')
+ttsLang('mb-es1')
+sleep(1)
+ttsSay('¿cuántos grados de libertad tiene mi cuerpo en total?')
+pause()
+
+ttsSay('tenéis razón, es verdad, los investigadores de roboticslab me hacen de todo')
+ttsSay('esto se puede apreciar en aquel cortometraje no nominado')
+ttsSay('llamado Sueño Profundo')
+ttsSay('hoy me han puesto esta garra')
+v = yarp.DVector(axesRA,0.0)
+v[0]=35
+v[1]=-35
+posRA.positionMove(v)
+while not posRA.checkMotionDone():
+    sleep(0.1)
+ttsSay('mañana, vé tú a saber')
+sleep(1)
+v = yarp.DVector(axesRA,0.0)
+posRA.positionMove(v)
+while not posRA.checkMotionDone():
+    sleep(0.1)
+
+ttsSay('juan, me rrindo, puedes soltarles')
+ttsLang('mb-en1')
+ttsSay('computing answer')
+ttsSay('complexity: infinite')
+ttsLang('mb-es1')
+sleep(1)
+ttsSay('cuarenta y dos')
+sleep(1)
+ttsSay('cuarenta y dos')
+sleep(1)
+ttsSay('cuarenta y dos')
+sleep(1)
 
